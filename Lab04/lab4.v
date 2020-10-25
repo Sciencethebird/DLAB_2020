a318a7e4e93c1c4d38d678f569ac0fb03d78c374
Birb <34442036+Sciencethebird@users.noreply.github.com>
2020-10-25 18:28:14 +0800

    code cleanup
    debouncing, counter

--- Lab04/lab4.v
+++ Lab04/lab4.v
@@ -7,6 +7,7 @@ module lab4(
 );
 
 `define DEBOUNCE_COUNT 10
+`define DUTY_CYCLE_LEN 20
 
 reg [3:0] usr_led;
 
@@ -20,15 +21,8 @@ integer BTN1_db_counter = 0;
 integer BTN2_db_counter = 0;
 integer BTN3_db_counter = 0;
 
-
 // BTN0, decreasing counter
-always @(posedge clk, posedge reset_n)begin
-    if(reset_n ==1)begin
-        BTN0_is_pressed <= 0;
-        BTN0_db_counter <= 0;
-        usr_led <= 0;
-    end
-    else begin
+always @(posedge clk)begin
         //$display("db counter: %d", BTN0_db_counter);
         if(usr_btn[0] && BTN0_db_counter == 0) begin
             $display("BTN0 pressed! decresing counter! %d", usr_btn);
@@ -40,17 +34,11 @@ always @(posedge clk, posedge reset_n)begin
         if (BTN0_db_counter >`DEBOUNCE_COUNT)begin
             BTN0_db_counter = 0;
             BTN0_is_pressed = 0;
-        end
-     end   
+        end 
 end
 
 // BTN1, increasing counter
-always @(posedge clk, posedge reset_n)begin
-    if(reset_n ==1)begin
-        BTN1_is_pressed <= 0;
-        BTN1_db_counter <= 0;
-    end
-    else begin
+always @(posedge clk)begin
         //$display("db counter: %d", BTN0_db_counter);
         if(usr_btn[1] && BTN1_db_counter == 0) begin
             $display("BTN1 pressed! incresing counter! %d", usr_btn);
@@ -62,8 +50,22 @@ always @(posedge clk, posedge reset_n)begin
         if (BTN1_db_counter >`DEBOUNCE_COUNT)begin
             BTN1_db_counter = 0;
             BTN1_is_pressed = 0;
-        end
-     end   
+        end  
+end
+
+// reset
+always @(posedge clk, posedge reset_n)begin
+    if(reset_n ==1)begin
+        BTN0_is_pressed <= 0;
+        BTN0_db_counter <= 0;
+        BTN1_is_pressed <= 0;
+        BTN1_db_counter <= 0;
+        BTN2_is_pressed <= 0;
+        BTN2_db_counter <= 0;
+        BTN3_is_pressed <= 0;
+        BTN3_db_counter <= 0;
+        usr_led <= 0;
+    end
 end
 
 endmodule
