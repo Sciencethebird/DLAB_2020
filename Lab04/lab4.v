bf7c216ed29adc85e6290a01bbe6775a43087efb
Birb <34442036+Sciencethebird@users.noreply.github.com>
2020-10-25 22:10:44 +0800

    complete pwm and all button debouncing
    Still don't know what will happen on the board though

--- Lab04/lab4.v
+++ Lab04/lab4.v
@@ -3,23 +3,36 @@ module lab4(
   input  clk,            // System clock at 100 MHz
   input  reset_n,        // System reset signal, in negative logic
   input  [3:0] usr_btn,  // Four user pushbuttons
-  output [3:0] usr_led   // Four yellow LEDs
+  output [3:0] usr_led,   // Four yellow LEDs
+  output pwm_signal, // my pwm test output
+  output [3:0]duty_idx // my duty cycle index
 );
 
-`define DEBOUNCE_COUNT 10
-`define DUTY_CYCLE_LEN 20
+`define DEBOUNCE_COUNT 150
+`define PWM_CYCLE_NUM 200 //200 *20ns = 4us
+`define DUTY_LEN 200*0.25
 
-reg [3:0] usr_led;
+//reg [3:0] usr_led;
+reg pwm_signal;
+reg led_pwm;
+reg [3:0] led_on;
 
 reg BTN0_is_pressed = 0;
 reg BTN1_is_pressed = 0;
 reg BTN2_is_pressed = 0;
 reg BTN3_is_pressed = 0;
 
-integer BTN0_db_counter = 0;
-integer BTN1_db_counter = 0;
-integer BTN2_db_counter = 0;
-integer BTN3_db_counter = 0;
+integer BTN0_db_counter;
+integer BTN1_db_counter;
+integer BTN2_db_counter;
+integer BTN3_db_counter;
+
+integer pwm_counter;
+integer duty_cycle_num[0:4];
+integer duty_cycle_idx;
+
+assign usr_led = led_on & {4{pwm_signal}};
+assign duty_idx = duty_cycle_idx;
 
 // BTN0, decreasing counter
 always @(posedge clk)begin
@@ -27,8 +40,8 @@ always @(posedge clk)begin
         if(usr_btn[0] && BTN0_db_counter == 0) begin
             $display("BTN0 pressed! decresing counter! %d", usr_btn);
             BTN0_is_pressed = 1;
-            if(usr_led>0) usr_led <= usr_led - 1; 
-            else usr_led <= 0;
+            if(led_on>0) led_on <= led_on - 1; 
+            else led_on <= 0;
         end
         if(BTN0_is_pressed) BTN0_db_counter <= BTN0_db_counter + 1;
         if (BTN0_db_counter >`DEBOUNCE_COUNT)begin
@@ -43,8 +56,8 @@ always @(posedge clk)begin
         if(usr_btn[1] && BTN1_db_counter == 0) begin
             $display("BTN1 pressed! incresing counter! %d", usr_btn);
             BTN1_is_pressed = 1;
-            if(usr_led>3)usr_led <= 3;
-            else  usr_led <= usr_led + 1;
+            if(led_on>= 15)led_on <= 15;
+            else  led_on <= led_on + 1;
         end
         if(BTN1_is_pressed) BTN1_db_counter <= BTN1_db_counter + 1;
         if (BTN1_db_counter >`DEBOUNCE_COUNT)begin
@@ -52,6 +65,47 @@ always @(posedge clk)begin
             BTN1_is_pressed = 0;
         end  
 end
+
+// BTN2, decrease brightness/pwm duty cycle
+always @(posedge clk)begin
+        //$display("db counter: %d", BTN0_db_counter);
+        if(usr_btn[2] && BTN2_db_counter == 0) begin
+            $display("BTN2 pressed! decresing duty cycle! %d", usr_btn);
+            BTN2_is_pressed = 1;
+            if(duty_cycle_idx>0) duty_cycle_idx <= duty_cycle_idx - 1;
+            else  duty_cycle_idx <= 0;
+        end
+        if(BTN2_is_pressed) BTN2_db_counter <= BTN2_db_counter + 1;
+        if (BTN2_db_counter >`DEBOUNCE_COUNT)begin
+            BTN2_db_counter = 0;
+            BTN2_is_pressed = 0;
+        end  
+end
+
+// BTN3, increase brightness(pwm)
+always @(posedge clk)begin
+        //$display("db counter: %d", BTN0_db_counter);
+        if(usr_btn[3] && BTN3_db_counter == 0) begin
+            $display("BTN3 pressed! incresing duty cycle! %d", usr_btn);
+            BTN3_is_pressed = 1;
+            if(duty_cycle_idx >= 4) duty_cycle_idx <= 4;
+            else  duty_cycle_idx <= duty_cycle_idx + 1; 
+        end
+        if(BTN3_is_pressed) BTN3_db_counter <= BTN3_db_counter + 1;
+        if (BTN3_db_counter >`DEBOUNCE_COUNT)begin
+            BTN3_db_counter = 0;
+            BTN3_is_pressed = 0;
+        end  
+end
+
+// pwm signal
+always @(posedge clk)begin
+    if(pwm_counter <= duty_cycle_num[duty_cycle_idx]) pwm_signal <=1;
+    else pwm_signal <=0;
+    if(pwm_counter >= `PWM_CYCLE_NUM) pwm_counter  <= 1;
+    else pwm_counter  <= pwm_counter +1;
+end
+
 
 // reset
 always @(posedge clk, posedge reset_n)begin
@@ -64,7 +118,14 @@ always @(posedge clk, posedge reset_n)begin
         BTN2_db_counter <= 0;
         BTN3_is_pressed <= 0;
         BTN3_db_counter <= 0;
-        usr_led <= 0;
+        led_on <= 0;
+        pwm_counter <= 1;
+        duty_cycle_num[0] <= `PWM_CYCLE_NUM*0.05;
+        duty_cycle_num[1] <= `PWM_CYCLE_NUM*0.25;
+        duty_cycle_num[2] <= `PWM_CYCLE_NUM*0.5;
+        duty_cycle_num[3] <= `PWM_CYCLE_NUM*0.75;
+        duty_cycle_num[4] <= `PWM_CYCLE_NUM;
+        duty_cycle_idx <= 4;
     end
 end
 
---
+++ Lab04/lab4_tb.v
@@ -0,0 +1,155 @@
+`timescale 1ns / 1ps
+module lab4_tb;
+
+reg clk, reset;
+
+reg  [3:0] usr_btn;
+wire  [3:0] usr_led;
+wire pwm_test;
+wire [3:0]duty_cycle_idx;
+wire l0, l1, l2, l3;
+
+
+lab4  lab4_test(.clk(clk), .reset_n(reset), .usr_btn(usr_btn), .usr_led(usr_led), .pwm_signal(pwm_test), .duty_idx(duty_cycle_idx));
+
+assign l0 = usr_led[0];
+assign l1 = usr_led[1];
+assign l2 = usr_led[2];
+assign l3 = usr_led[3];
+
+`define strobe 20
+`define press_delay 2000
+`define rest 5000
+`define INC_COUNTER 4'b0010
+`define DEC_COUNTER 4'b0001
+`define INC_PWM 4'b1000
+`define DEC_PWM 4'b0100
+
+// set clk
+initial   clk = 0;
+always #(`strobe/2) clk = ~clk;
+
+// pattern generate
+  initial
+    begin
+        
+        reset = 1;
+        # `press_delay;
+        reset = 0;
+        # `press_delay;       
+        usr_btn = `INC_COUNTER; //counter = 1
+        # `press_delay;
+        RELEASE_BUTTON;      
+        usr_btn = `INC_COUNTER; //counter = 2
+        # `press_delay;
+        RELEASE_BUTTON;      
+        usr_btn = `INC_COUNTER; //counter = 3
+        # `press_delay;
+        RELEASE_BUTTON;   
+        usr_btn = `INC_COUNTER; //counter = 4
+        # `press_delay;
+        RELEASE_BUTTON;       
+        usr_btn = `INC_COUNTER; //counter = 5
+        # `press_delay;
+        RELEASE_BUTTON;     
+        usr_btn = `INC_COUNTER; //counter = 6
+        # `press_delay;
+        RELEASE_BUTTON;       
+        usr_btn = `INC_COUNTER; //counter = 7
+        # `press_delay;
+        RELEASE_BUTTON;      
+        usr_btn = `INC_COUNTER; //counter = 8
+        # `press_delay;
+        RELEASE_BUTTON;        
+        usr_btn = `INC_COUNTER; //counter = 9
+        # `press_delay;
+        RELEASE_BUTTON;       
+        usr_btn = `INC_COUNTER; //counter = 10
+        # `press_delay;
+        RELEASE_BUTTON;       
+        usr_btn = `INC_COUNTER; //counter = 11
+        # `press_delay;
+        RELEASE_BUTTON;       
+        usr_btn = `INC_COUNTER; //counter = 12
+        # `press_delay;
+        RELEASE_BUTTON;
+        usr_btn = `INC_COUNTER; //counter = 13
+        # `press_delay;
+        RELEASE_BUTTON;
+         usr_btn = `INC_COUNTER; //counter = 14
+        # `press_delay;
+        RELEASE_BUTTON;
+        usr_btn = `INC_COUNTER; //counter = 15
+        # `press_delay;
+        RELEASE_BUTTON;
+        
+        usr_btn = `DEC_COUNTER; //counter = 14
+        # `press_delay;
+        RELEASE_BUTTON;
+        
+        usr_btn = `DEC_COUNTER; //counter = 13
+        # `press_delay;
+        RELEASE_BUTTON;
+        
+        usr_btn = `DEC_COUNTER; //counter = 12
+        # `press_delay;
+        RELEASE_BUTTON;
+        
+        usr_btn = `DEC_COUNTER; //counter = 11
+        # `press_delay;
+        RELEASE_BUTTON;
+        
+        usr_btn = `INC_COUNTER; //counter = 12
+        # `press_delay;
+        RELEASE_BUTTON;
+        
+        usr_btn = `DEC_PWM; //counter = 12, pwm = 75
+        # `press_delay;
+        RELEASE_BUTTON;
+        
+        usr_btn = `DEC_PWM; //counter = 12, pwm = 50
+        # `press_delay;
+        RELEASE_BUTTON;
+        
+        usr_btn = `DEC_PWM; //counter = 12, pwm = 25
+        # `press_delay;
+        RELEASE_BUTTON;
+        
+        usr_btn = `DEC_PWM; //counter = 12, pwm = 5
+        # `press_delay;
+        RELEASE_BUTTON;
+        
+        usr_btn = `DEC_PWM;//counter = 12, pwm = 5
+        # `press_delay;
+        RELEASE_BUTTON;
+        
+         usr_btn = `INC_PWM;//counter = 12, pwm = 25
+        # `press_delay;
+        RELEASE_BUTTON;
+        
+        usr_btn = `INC_PWM;//counter = 12, pwm = 50
+        # `press_delay;
+        RELEASE_BUTTON;
+        
+        usr_btn = `INC_COUNTER; //counter = 13
+        # `press_delay;
+        RELEASE_BUTTON;
+        
+        usr_btn = `INC_COUNTER; //counter = 14
+        # `press_delay;
+        RELEASE_BUTTON;
+        
+         usr_btn = `INC_PWM;//counter = 14, pwm = 75
+        # `press_delay;
+        RELEASE_BUTTON;
+        # `rest;
+        $display("Simulation Done!");
+    end
+    
+ task RELEASE_BUTTON;
+    begin
+        usr_btn=4'b0000;
+        # `press_delay;
+    end
+  endtask
+endmodule
